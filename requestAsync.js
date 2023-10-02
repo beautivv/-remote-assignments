4fbc564bcb18dfb59cbd6973a1d803fe461e64c2
beautivv <vv020423lin@gmail.com>
2023-10-02 16:13:16 +0800

    remote-week1: hw _v3

--- requestAsync.js
+++ requestAsync.js
@@ -1,16 +1,56 @@
-const axios = require('axios');   //使用了 axios 的 get 函數來發送 GET 請求
+const https = require('https');
+const axios = require('axios'); //使用了 axios 的 get 函數來發送 GET 請求
 
 const url = "https://ec2-54-64-246-136.ap-northeast-1.compute.amazonaws.com/delay-clock";
 
-console.log("Async Function"); 
+function requestCallback(url, callback) {
+    const startTime = new Date().getTime();
+    https.get(url, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
 
-async function requestAsync(url) {
+        res.on('end', () => {
+            const endTime = new Date().getTime();
+            console.log(`Callback 的執行時間: ${endTime - startTime} ms`);
+            callback(data);
+            // console.log("確定有執行到此function(debug用)") 
+        });
+    });
+}
+
+function requestPromise(url) {
+    return new Promise((resolve, reject) => {
+        const startTime = new Date().getTime();
+        https.get(url, (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                const endTime = new Date().getTime();
+                console.log(`Promise 的執行時間 : ${endTime - startTime} ms`);
+                resolve(data);
+            });
+        })
+    });
+}
+
+async function requestAsyncAwait(url) {
     const startTime = new Date().getTime();
         const response = await axios.get(url);
         const endTime = new Date().getTime();
-        console.log(`執行秒數: ${endTime - startTime} ms`);
+        console.log(`Async/Await 的執行時間: ${endTime - startTime} ms`);
+    
 }
 
-requestAsync(url); 
-requestAsync(url);
-requestAsync(url);
+
+requestCallback(url, (data) => {
+});
+
+requestPromise(url).then((data) => {
+})
+
+requestAsyncAwait(url);
--- requestSync.js
+++ requestSync.js
@@ -1,16 +1,22 @@
-const axios = require('axios');  //使用了 axios 的 get 函數來發送 GET 請求
+const https = require('https');
 
 const url = "https://ec2-54-64-246-136.ap-northeast-1.compute.amazonaws.com/delay-clock";
 
-console.log("Sync Function"); // 紀錄（方便閱讀）
-
-async function requestSync(url) {
+function requestSync(url) {
     const startTime = new Date().getTime();
-        await axios.get(url);  //使用 await 構造來等待非同步操作的完成
-        const endTime = new Date().getTime();
-        console.log(`執行秒數: ${endTime - startTime} ms`);
+    const response = https.get(url, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            const endTime = new Date().getTime();
+            console.log(`Sync Function 執行時間: ${endTime - startTime} ms`);
+        });
+    });
 }
 
-requestSync(url); 
+requestSync(url);
 requestSync(url);
 requestSync(url);
